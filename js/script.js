d1921ff9db9a0fbafa3c28f63d918cf6cecda322
Tristan Armitage <armitageth@gmail.com>
2020-11-23 23:27:01 -0500

    Added content from business plan

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 
-window.addEventListener('scroll', function(e) => {
+window.addEventListener('scroll', function(e) {
   console.log(document.documentElement.scrollTop);
-  console.log(document.documentElement.scrollTop);
+});
+
+var panes = Array.from(document.getElementsByClassName('pane'));
+console.log(panes);
+panes = panes.filter( (e) => e.id != 'splash' );
+console.log(panes);
+panes.forEach((e, i) => {
+  Array.from(e.children).forEach((c, j) => {
+    // c.classList.add('unshown');
+  });
 });
